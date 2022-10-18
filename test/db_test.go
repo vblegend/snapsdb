fce879bb952d56c61891697c5edfa39939c0f0b6
vblegend <liu.yandong.hanks@outlook.com>
2022-10-18 19:55:13 +0800

    test

--- test/db_test.go
+++ test/db_test.go
@@ -3,7 +3,10 @@ package test
 import (
 	"bytes"
 	"fmt"
+	"log"
+	"os"
 	"path/filepath"
+	"syscall"
 
 	"testing"
 	"time"
@@ -139,4 +142,28 @@ func TestXxx(t *testing.T) {
 	fmt.Println("=============================================")
 	snapsdb.Traverse(data, "")
 
+}
+
+func TestMmap(t *testing.T) {
+	f, err := os.OpenFile("mmap.bin", os.O_RDWR|os.O_CREATE, 0644)
+	if nil != err {
+		log.Fatalln(err)
+	}
+	// extend file
+	if _, err := f.WriteAt([]byte{byte(0)}, 1<<8); nil != err {
+		log.Fatalln(err)
+	}
+	data, err := syscall.Mmap(int(f.Fd()), 0, 1<<8, syscall.PROT_WRITE, syscall.MAP_SHARED)
+	if nil != err {
+		log.Fatalln(err)
+	}
+	if err := f.Close(); nil != err {
+		log.Fatalln(err)
+	}
+	for i, v := range []byte("hello syscall") {
+		data[i] = v
+	}
+	if err := syscall.Munmap(data); nil != err {
+		log.Fatalln(err)
+	}
 }
