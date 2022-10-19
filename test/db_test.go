a1cdf2c2748a741b04ae331a995a3f1b935ba5b5
vblegend <liu.yandong.hanks@outlook.com>
2022-10-19 09:18:46 +0800

    mmap

--- test/db_test.go
+++ test/db_test.go
@@ -3,7 +3,9 @@ package test
 import (
 	"bytes"
 	"fmt"
+	"os"
 	"path/filepath"
+	"syscall"
 
 	"testing"
 	"time"
@@ -34,6 +36,43 @@ func InitDB() snapsdb.SnapsDB {
 		panic(err)
 	}
 	return db
+}
+
+const maxMapSize = 0x8000000000
+const maxMmapStep = 1 << 30 // 1GB
+
+func mmapSize(size int) (int, error) {
+	// Double the size from 32KB until 1GB.
+	for i := uint(15); i <= 30; i++ {
+		if size <= 1<<i {
+			return 1 << i, nil
+		}
+	}
+
+	// Verify the requested size is not above the maximum allowed.
+	if size > maxMapSize {
+		return 0, fmt.Errorf("mmap too large")
+	}
+
+	// If larger than 1GB then grow by 1GB at a time.
+	sz := int64(size)
+	if remainder := sz % int64(maxMmapStep); remainder > 0 {
+		sz += int64(maxMmapStep) - remainder
+	}
+
+	// Ensure that the mmap size is a multiple of the page size.
+	// This should always be true since we're incrementing in MBs.
+	pageSize := int64(os.Getpagesize())
+	if (sz % pageSize) != 0 {
+		sz = ((sz / pageSize) + 1) * pageSize
+	}
+
+	// If we've exceeded the max size then only grow up to the max size.
+	if sz > maxMapSize {
+		sz = maxMapSize
+	}
+
+	return int(sz), nil
 }
 
 // 测试 snapshotDB 批量插入
@@ -139,4 +178,41 @@ func TestXxx(t *testing.T) {
 	fmt.Println("=============================================")
 	snapsdb.Traverse(data, "")
 
+}
+
+func TestFileStream(t *testing.T) {
+	file, err := os.OpenFile("my.db", os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	stat, err := os.Stat("my.db")
+	if err != nil {
+		panic(err)
+	}
+
+	size, err := mmapSize(int(stat.Size()))
+	if err != nil {
+		panic(err)
+	}
+
+	b, err := syscall.Mmap(int(file.Fd()), 0, size, syscall.PROT_WRITE|syscall.PROT_READ, syscall.MAP_SHARED)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(len(b))
+	defer file.Close()
+	err = syscall.Ftruncate(int(file.Fd()), int64(size))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(len(b))
+	for index, bb := range []byte("Hello world") {
+		b[index] = bb
+	}
+
+	err = syscall.Munmap(b)
+	if err != nil {
+		panic(err)
+	}
 }
